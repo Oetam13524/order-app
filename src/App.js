8825e9f15eb16bbb116399f1842fd0d6bedf0860
Mateo <mateo@gmail.com>
2022-05-23 09:55:57 +0200

    merged cart feature

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
   }
   return (
     <CartProvider>
-      <Cart />
-      { cartIsShown && <div>Cart...</div> }
+      { cartIsShown && <Cart onlose={hideCartHandler} /> }
       <Header onShowCart = {showCartHandler} />
       <main>
         <Meals />
